0996c25eae0d75112a2ec8f2ab990bba6055bedc
Gabriel Vieira <75680725+gabriel-vieira06@users.noreply.github.com>
2022-04-08 21:34:36 -0300

    update gauss.c

--- gauss.c
+++ gauss.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define EPSILON 0.000001
+
 double** criaMatriz ( int l, int c );
 void lerMatriz ( double** m, int l, int c );
 void exibeMatriz ( double** m, int l, int c );
 int substituicaoRetroativa ( double** m, int n, double x[] );
+void Gauss ( double** m, int n );
 
 int main()
 {
@@ -19,26 +22,34 @@ int main()
 	m = criaMatriz ( n , n + 1 );
 	x = malloc ( sizeof ( double ) * n );
 	
-	if ( m == NULL || x == NULL )	// Faltou memória, menó
+	if ( m == NULL || x == NULL )	// Faltou memoria, meno
 	{
 		printf ( "Deu Pau!\n Falta de Memoria\n" );
 		return 1;
 	}
 	
 	lerMatriz ( m , n , n + 1 );
+
+	printf ( "\tMatriz Quadrada:\n" );
 	exibeMatriz ( m , n , n + 1 );
+
+	Gauss ( m , n );
+
+	printf ( "\tMatriz Triangular Superior:\n" );
+	exibeMatriz ( m , n , n + 1 );
+
 	tipo = substituicaoRetroativa ( m , n , x );
 	
 	if ( tipo == 2 )
 	{
-		printf ( "Sistema Linear Incompativel. \n" );
+		printf ( "\tSistema Linear Incompativel. \n" );
 	}
 	else
 	{
-		printf ( "Sistema Linear %sdeterminado. \n", tipo ? "in" : "" );
+		printf ( "\tSistema Linear %sdeterminado. \n\n", tipo ? "in" : "" );
 		for ( i = 0; i < n ; i++ )
 		{
-			printf( "x[%d] = %10.3lf\n", i + 1 , x [ i ] );
+			printf( "\tx[%d] = %10.3lf\n", i + 1 , x [ i ] );
 		}
 	}
 	
@@ -50,9 +61,9 @@ double** criaMatriz ( int l, int c )
 {
 	
 /* 	
-	Se houver memória disponível, cria uma matriz de double
+	Se houver memoria disponivel, cria uma matriz de double
 	com l linha e c colunas e devolve um ponteiro para a matriz;
-	Caso contrário, devolve um ponteiro nulo.
+	Caso contrario, devolve um ponteiro nulo.
 */
 
 	int i, j;
@@ -60,7 +71,7 @@ double** criaMatriz ( int l, int c )
 	
 	m = malloc ( sizeof ( double *) * l );
 	
-	if ( m == NULL )		// Falta de memória.
+	if ( m == NULL )		// Falta de memoria.
 	{
 		return NULL;
 	}
@@ -70,7 +81,7 @@ double** criaMatriz ( int l, int c )
 		
 		m [ i ] = malloc ( sizeof ( double ) * c );
 		
-		if ( m [ i ] == NULL )		// Falta de memória.
+		if ( m [ i ] == NULL )		// Falta de memoria.
 		{
 		
 			for ( j = 0 ; j < i ; j++ )
@@ -107,6 +118,8 @@ void lerMatriz ( double** m, int l, int c )
 		}
 	}
 	
+	printf ( "\n" );
+
 }	// Fim lerMatriz.
 
 void exibeMatriz ( double** m, int l, int c )
@@ -118,7 +131,9 @@ void exibeMatriz ( double** m, int l, int c )
 	*/
 	
 	int i, j;
-	
+
+	printf ( "\n" );
+
 	for ( i = 0 ; i < l ; i++ )
 	{
 		for ( j = 0 ; j < c ; j++ )
@@ -129,6 +144,8 @@ void exibeMatriz ( double** m, int l, int c )
 		printf ( "\n" );
 		
 	}
+
+	printf ( "\n" );
 	
 }	// Fim exibeMatriz
 
@@ -138,11 +155,11 @@ int substituicaoRetroativa ( double** m, int n, double x[] )
 	/*
 		Recebe m, a matriz aumentada de um Sistema Linear Triangular Superior
 		com n variaveis; 
-		Se o Sistema Linear for determinado, coloca em x a solução
+		Se o Sistema Linear for determinado, coloca em x a solucao
 		do SL e devolve 0;
-		Se o Sistema Linear for indeterminado, coloca em x uma solução
+		Se o Sistema Linear for indeterminado, coloca em x uma solucao
 		do SL e devolve 1;
-		Se o Sistema Linear for imcompativel, devolve 2;
+		Se o Sistema Linear for imcompativel, devolve 2.
 	*/
 	
 	int i, j, tipo = 0;
@@ -159,12 +176,12 @@ int substituicaoRetroativa ( double** m, int n, double x[] )
 		
 		if ( m [ i ][ i ] == 0 )
 		{
-			if ( soma == m [ i ][ n ] )		// x[i] é variável livre.
+			if ( fabs ( m [ i ] [ n ] - soma ) < EPSILON )		// x[i] eh variavel livre.
 			{
 				x [ i ] = 0;
 				tipo = 1;
 			}
-			else return 2;		// Sistema Linear Incompatível
+			else return 2;		// Sistema Linear Incompativel
 		}
 		else x [ i ] = ( m [ i ][ n ] - soma ) / m [ i ][ i ];
 		
@@ -173,3 +190,49 @@ int substituicaoRetroativa ( double** m, int n, double x[] )
 	return tipo;
 
 }	// Fim substituicaoRetroativa 
+
+void Gauss ( double** m, int n )
+{
+
+	/*
+		Recebe m, a matriz aumentada de um Sistema Linear
+		com n variaveis e n equacoes, e transforma m na matriz
+		aumentada de um Sistema Linear Triagular Superior equivalente.
+	*/
+
+	int i, j, k;
+	double multiplicador;
+	double* auxiliar;
+
+	for ( i = 0 ; i < n - 1 ; i++ )
+	{
+		if ( m [ i ] [ i ] == 0 )	// Pivo nulo
+		{
+			j = i + 1;
+			while ( j < n && m [ j ] [ i ] == 0 )
+			{
+				j++;
+			}
+			if ( j < n )	// Troca das linhas i e j
+			{
+				printf ( "\tGauss: Trocando linhas!\n" );
+				auxiliar = m [ i ];
+				m [ i ] = m [ j ];
+				m [ j ] = auxiliar;
+			} 
+		}
+
+		if ( m [ i ] [ i ] != 0 )
+		{
+			for ( j = i + 1 ; j < n ; j++ )
+			{
+				multiplicador = - m [ j ] [ i ] / m [ i ] [ i ];
+				m [ j ] [ i ] = 0;
+				for ( k = i + 1 ; k <= n ; k++ )
+				{
+					m [ j ] [ k ] += multiplicador * m [ i ] [ k ];
+				}
+			}
+		}
+	}
+}	// Fim Gauss
